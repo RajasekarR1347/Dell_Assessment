a5549f3c26dbcf062a987015633aecc54ec2eaa2
RajasekarR1347 <rajasekar1722@gmail.com>
2022-11-06 20:14:22 +0100

    assessment

---
+++ schema.py
@@ -0,0 +1,33 @@
+class schemas():
+	people_schema= {
+			"name": {"type": "string"},
+			"height":"type": "number"},
+			"mass": {"type": "number"},
+			"hair_color": {"type": "string"},
+			"skin_color": {"type": "string"},
+			"eye_color": {"type": "string"},
+			"birth_year": "19BBY",
+			"gender": "type": "number"},
+			"homeworld": "https://swapi.py4e.com/api/planets/1/",
+			"films": [
+				"https://swapi.py4e.com/api/films/1/",
+				"https://swapi.py4e.com/api/films/2/",
+				"https://swapi.py4e.com/api/films/3/",
+				"https://swapi.py4e.com/api/films/6/",
+				"https://swapi.py4e.com/api/films/7/"
+			],
+			"species": [
+				"https://swapi.py4e.com/api/species/1/"
+			],
+			"vehicles": [
+				"https://swapi.py4e.com/api/vehicles/14/",
+				"https://swapi.py4e.com/api/vehicles/30/"
+			],
+			"starships": [
+				"https://swapi.py4e.com/api/starships/12/",
+				"https://swapi.py4e.com/api/starships/22/"
+			],
+			"created": "2014-12-09T13:50:51.644000Z",
+			"edited": "2014-12-20T21:17:56.891000Z",
+			"url": "https://swapi.py4e.com/api/people/1/"
+		},
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import json
 from datetime import datetime
 import requests
 import pytest
 import time
+from cerberus import Validator
+from schema import schemas
 
 url = requests.get("https://swapi.py4e.com/api/people/")
 number_0 = requests.get("https://swapi.py4e.com/api/people/0")
@@ -29,6 +32,10 @@ def test_get_user():
       people.append(tot_user)
     return people
 
+@get_time
+def test_read_one_operation_has_expected_schema():
+   response = requests.get(f'{}/1')
+   person = json.loads(response.text)
 
 def unique_names(l):
     return list(set([x for x in l if l.count(x) > 1]))
@@ -49,5 +56,14 @@ def test_page_number_0():
     status=url1.status_code
     assert status == 404
 
+'''
+⦁	create test(s) which validate that all people objects contain required schema fields.
+If validation fails – person id and name should be in error/fail message.
+All persons with failed validation must be reported during one test run.
+⦁	create factory fixture “search_in_resource” that returns search function depending on the resource name provided as a parameter (people, planet, etc)
+⦁	create test which checks that search for any char in English alphabet or any number from 0 to 9 returns number of results>0 except cases of search by 6, 9 and 0. It is not allowed to use loops inside the test body.
+
+
+'''
 
 
